0cbdff28299b527c72fd848f5e00249d5954f7fb
Rezis Github <cosmonshop@gmail.com>
2026-02-15 18:45:17 +0400

    Create kewords.js

---
+++ utils/kewords.js
@@ -0,0 +1,14 @@
+export function getKeywords() {
+  return [
+    "led lamp",
+    "mini gadget",
+    "home smart",
+    "pet toy",
+    "car accessory",
+    "kitchen gadget",
+    "TELESCOPES",
+    "optics",
+    "travel gadget",
+    "decor light"
+  ];
+}
